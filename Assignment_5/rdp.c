f752e297f246db886e2c4bdab9182232e760cc74
Sakshi Rajale <149033491+Sakshirajale@users.noreply.github.com>
2025-06-05 19:13:59 +0530

    Add files via upload

---
+++ Assignment_5/rdp.c
@@ -0,0 +1,126 @@
+   /* Write a program to implement a Recursive Descent Parser (RDP) for a 
+simple grammar. 
+We’ll use a basic grammar for arithmetic expressions like: 
+E → T E' 
+E' → + T E' | ε 
+T → F T' 
+T' → * F T' | ε 
+F → (E) | id 
+
+This grammar can parse expressions like: id + id * id 
+• E is the starting symbol 
+• ε (epsilon) represents an empty string 
+• id is a valid identifier (e.g., a, x, etc.) */
+
+   #include <stdio.h>  
+    #include <string.h>  
+      
+    #define SUCCESS 1  
+    #define FAILED 0  
+      
+    int E(), Edash(), T(), Tdash(), F();  
+      
+    const char *cursor;  
+    char string[64];  
+      
+    int main()  
+    {  
+        puts("Enter the string");  
+        // scanf("%s", string);  
+        sscanf("i+(i+i)*i", "%s", string);  
+        cursor = string;  
+        puts("");  
+        puts("Input      Action");  
+        puts("--------------------------------");  
+      
+        if (E() && *cursor == '\0') {  
+            puts("--------------------------------");  
+            puts("String is successfully parsed");  
+            return 0;  
+        } else {  
+            puts("--------------------------------");  
+            puts("Error in parsing String");  
+            return 1;  
+        }  
+    }  
+      
+    int E()  
+    {  
+        printf("%-16s E -> T E'\n", cursor);  
+        if (T()) {  
+            if (Edash())  
+                return SUCCESS;  
+            else  
+                return FAILED;  
+        } else  
+            return FAILED;  
+    }  
+      
+    int Edash()  
+    {  
+        if (*cursor == '+') {  
+            printf("%-16s E' -> + T E'\n", cursor);  
+            cursor++;  
+            if (T()) {  
+                if (Edash())  
+                    return SUCCESS;  
+                else  
+                    return FAILED;  
+            } else  
+                return FAILED;  
+        } else {  
+            printf("%-16s E' -> $\n", cursor);  
+            return SUCCESS;  
+        }  
+    }  
+      
+    int T()  
+    {  
+        printf("%-16s T -> F T'\n", cursor);  
+        if (F()) {  
+            if (Tdash())  
+                return SUCCESS;  
+            else  
+                return FAILED;  
+        } else  
+            return FAILED;  
+    }  
+      
+    int Tdash()  
+    {  
+        if (*cursor == '*') {  
+            printf("%-16s T' -> * F T'\n", cursor);  
+            cursor++;  
+            if (F()) {  
+                if (Tdash())  
+                    return SUCCESS;  
+                else  
+                    return FAILED;  
+            } else  
+                return FAILED;  
+        } else {  
+            printf("%-16s T' -> $\n", cursor);  
+            return SUCCESS;  
+        }  
+    }  
+      
+    int F()  
+    {  
+        if (*cursor == '(') {  
+            printf("%-16s F -> ( E )\n", cursor);  
+            cursor++;  
+            if (E()) {  
+                if (*cursor == ')') {  
+                    cursor++;  
+                    return SUCCESS;  
+                } else  
+                    return FAILED;  
+            } else  
+                return FAILED;  
+        } else if (*cursor == 'i') {  
+            cursor++;  
+            printf("%-16s F ->i\n", cursor);  
+            return SUCCESS;  
+        } else  
+            return FAILED;  
+    }  
